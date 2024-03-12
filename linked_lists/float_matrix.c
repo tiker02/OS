26b7ef70aae08d8fefc4b4ce9350dd42f0b352fc
tiker <angelo.jedy@gmail.com>
2024-03-12 09:34:56 +0100

    Float_matrix source correction completed

--- linked_lists/float_matrix.c
+++ linked_lists/float_matrix.c
@@ -1,43 +1,47 @@
+#include <stdlib.h>
 #include "float_matrix.h" 
 
 
 void FloatMatrixInit(MatrixHead* head)
 {
-    head->list = malloc(sizeof(ListHead));
-    List_init(head->list);
-    ListItem* prev = head->list->last;
+    List_init( head);
     for(int i = 0; i < head->rows; i++)
     {
-        MatrixRow* row = malloc(sizeof(MatrixRow));
-        row->row = malloc(sizeof(ListHead));
-        List_insert(head, prev, row);
-        prev = prev = head->list->last;
-
+        MatrixRow* row_it = malloc(sizeof(MatrixRow));
+        row_it->list.prev = NULL;
+        row_it->list.next = NULL;
+        row_it->row = malloc(sizeof(ListHead));
+        List_insert( head, head->list.last,  row_it);
+        List_init(row_it->row);
+        
         for(int j = 0; j < head->cols; j++)
         {
-            List_init(row->row);
-
             FloatListItem* new_it = malloc(sizeof(FloatListItem));
-            new_it->info = (float)(i + j/10);
-            List_insert(row->row, row->row->last, new_it); 
+            new_it->info = (float)(i + j*0.1);
+            new_it->list.prev = NULL; 
+            new_it->list.next = NULL;
+            List_insert(row_it->row, row_it->row->last,  new_it);
         }
+        
+        //FloatListItem* second_float_it = (FloatListItem*) second_in_list;
+        //printf("%0.2f\n", second_float_it->info);
     }
 }
 
 void FloatMatrixDelete(MatrixHead* matrix)
 {
-    MatrixRow* row = matrix->list->first;
-    while(row)
+    MatrixRow* row_it = matrix->list.first;
+    while(row_it)
     {
-        while(row->row->first)
+        while(row_it->row->first)
         {
-            free(List_detach(row->row, row->row->first));
+            free(List_detach(row_it->row, row_it->row->first));
         }
-        free(row->row);
+        free(row_it->row);
+        free(List_detach( matrix,  row_it));
 
-        free(List_detach(matrix->list, row));
+        row_it = (MatrixRow*)  matrix->list.first;
     }
-    free(matrix->list);
     free(matrix);
 }
 
@@ -47,18 +51,18 @@ void FloatList_print(ListHead* list)
     while(f_it)
     {
         printf("%0.2f ", f_it->info);
-        f_it = f_it->list->next;
+        f_it = f_it->list.next;
     }
     printf("\n");
 }
 
 void FloatMatrix_print(MatrixHead* matrix)
 {
-    MatrixRow* row = matrix->list->first;
+    MatrixRow* row = matrix->list.first;
     while(row)
     {
         FloatList_print(row->row);
-        row = row->list->next;
+        row = row->list.next;
     }
 }
 
@@ -67,7 +71,7 @@ ListHead* perRowSum(MatrixHead* matrix)
     ListHead* SumList = malloc(sizeof(ListHead));
     List_init(SumList);
 
-    MatrixRow* row = matrix->list->first;
+    MatrixRow* row = matrix->list.first;
     while(row)
     {
         float sum = 0;
@@ -75,14 +79,16 @@ ListHead* perRowSum(MatrixHead* matrix)
         while(f_it)
         {
             sum += f_it->info;
-            f_it = f_it->list->next;
+            f_it = f_it->list.next;
         }
 
         FloatListItem* sum_it = malloc(sizeof(FloatListItem));
+        sum_it->list.next = NULL;
+        sum_it->list.prev = NULL;
         sum_it->info = sum;
         List_insert(SumList, SumList->last, sum_it);
 
-        row = row->list->next; 
+        row = row->list.next; 
     }
 
     return SumList;
@@ -96,6 +102,6 @@ int main(int  argc, char* argv[])
     m->cols = 5;
     FloatMatrixInit(m);
     FloatMatrix_print(m);
-    FloatMatrix_print(perRowSum(m));
+    FloatList_print(perRowSum(m));
     FloatMatrixDelete(m);
 }
